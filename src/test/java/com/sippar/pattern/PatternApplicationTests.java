70c72104b76e583f1ea1b256915ba1d0e1d6e5b4
chavesef <efchaves@uolinc.com>
2024-07-05 16:47:35 -0300

    feat: creating project

---
+++ src/test/java/com/sippar/pattern/PatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sippar.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
